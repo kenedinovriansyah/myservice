01fdd241540fb7fbd835da0c0a85e32cb6f30512
kenedi <kenedi@kenedis-MacBook-Air.local>
2021-09-29 06:01:27 +0700

    add default component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DefaultComponent } from './default/default.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  component: DefaultComponent,
+  path: '',
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
